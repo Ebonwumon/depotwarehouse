6d708419521ce70c931b445d9af18af61821b2f0
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-07-08 06:20:54 -0600

    Updated blog view

--- public/themes/blocky/layout.blade.php
+++ public/themes/blocky/layout.blade.php
@@ -3,7 +3,7 @@
 @section('imports')
 <script src="/vendor/bower_components/platform/platform.js"></script>
 <link rel="import" href="/components/post-card.html">
-<link rel="import" href="/components/nav-card.html">
+<link rel="import" href="/components/nav-panel.html">
 @stop
 
 @section('content')
@@ -11,7 +11,13 @@
     <div class="pure-g">
         <div class="pure-u-1 pure-u-md-1-3">
             <div style="position:relative;">
-                <nav-card></nav-card>
+                <nav-panel>
+                    <ul>
+                        <li>
+                            <a href="{{ Wardrobe::route('posts.archive') }}">Archive</a>
+                        </li>
+                    </ul>
+                </nav-panel>
             </div>
 
         </div>
