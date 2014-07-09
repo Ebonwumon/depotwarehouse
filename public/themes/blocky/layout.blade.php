04ae7e4beaf09f4344ea648f16c12d679a672830
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-07-09 04:37:51 -0600

    updated stylins

--- public/themes/blocky/layout.blade.php
+++ public/themes/blocky/layout.blade.php
@@ -8,24 +8,15 @@
 
 @section('content')
 <div class="blog-container" layout vertical center>
-    <div class="pure-g">
-        <div class="pure-u-1 pure-u-md-1-3">
-            <div style="position:relative;">
-                <nav-panel>
-                    <ul>
-                        <li>
-                            <a href="{{ Wardrobe::route('posts.archive') }}">Archive</a>
-                        </li>
-                    </ul>
-                </nav-panel>
-            </div>
-
-        </div>
-        <div class="pure-u-1 pure-u-md-2-3">
-            @yield('blog_content')
-        </div>
-
-
+    <nav-panel>
+        <ul>
+            <li>
+                <a href="{{ Wardrobe::route('posts.archive') }}">Archive</a>
+            </li>
+        </ul>
+    </nav-panel>
+    <div class="post-container">
+        @yield('blog_content')
     </div>
 </div>
 
